7675bb9a3fa4862a49a03b3771d915846f8a847d
GentleDelirium <ivan.pyzhin@gmail.com>
2026-03-03 14:40:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#Hello
+import mysql.connector
+import math
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='game_project',
+    user='boris',
+    password='Bubalar60',
+    autocommit=True
+)
+
+def geo_to_deg(lat1,lon1,lat2,lon2):
